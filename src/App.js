bf790927abaa1f6844a6439a6ca7bb9d426394ed
Deborah Vieira <deborahanalistati@gmail.com>
2020-09-01 16:34:44 -0300

    “Componente-Filtro”

--- src/App.js
+++ src/App.js
@@ -1,10 +1,101 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React, { Component } from "react";
+import Axios from "axios";
 
-function App() {
-	return (
-        <AppContainer />
-	)
+const baseURL =
+  "https://us-central1-labenu-apis.cloudfunctions.net/futureCarTwo";
+export default class App extends Component {
+  state = {
+    inputMax: "",
+    inputMin: "",
+    inputSearch: "",
+    carArray: [],
+    showFilter: false,
+  };
+
+  componentDidMount() {
+    this.getAllCars();
+  }
+
+  getAllCars = async () => {
+    try {
+      const response = await Axios.get(`${baseURL}/cars`);
+      this.setState({ carArray: response.data.cars });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleInputMax = (e) => {
+    this.setState({ inputMax: e.target.value });
+  };
+
+  handleInputMin = (e) => {
+    this.setState({ inputMin: e.target.value });
+  };
+
+  handleInputSearch = (e) => {
+    this.setState({ inputSearch: e.target.value });
+  };
+
+  changeFilter = () => {
+    this.setState({ showFilter: !this.state.showFilter });
+  };
+
+  render() {
+    const carList = this.state.carArray
+      .filter((item) => {
+        return item.name.toLowerCase().indexOf(this.state.inputSearch) >= 0;
+      })
+      .filter((item) => {
+        return item.price >= (parseFloat(this.state.inputMin) || 0);
+      })
+      .filter((item) => {
+        return item.price <= (parseFloat(this.state.inputMax) || Infinity);
+      })
+
+      .map((car) => {
+        return (
+          <div key={car.id}>
+            <p>{car.name}</p>
+            <p>{car.price}</p>
+            <p>{car.description}</p>
+            <p>{car.paymt}</p>
+            <p>{car.shipping}</p>
+          </div>
+        );
+      });
+
+    return (
+      <>
+        <button onClick={this.changeFilter}>Filtros</button>
+        {this.state.showFilter && (
+          <div>
+            <label htmlFor="valueMax">Valor Máximo:</label>
+            <input type="number" id="valueMax" onChange={this.handleInputMax} />
+            <label htmlFor="valueMin">Valor Mínimo:</label>
+            <input type="number" id="valueMin" onChange={this.handleInputMin} />
+          </div>
+        )}
+
+        <label htmlFor="SearchName">Buscar produto:</label>
+        <input id="SearchName" onChange={this.handleInputSearch} />
+        <br />
+        <br />
+        <button>Vender</button>
+        <br />
+        <br />
+
+        <select>
+          <option>Ordenar por</option>
+          <option>Título</option>
+          <option>Valor da venda</option>
+          <option>Prazo de entrega</option>
+        </select>
+        <br />
+        <br />
+        <br />
+        {carList}
+      </>
+    );
+  }
 }
-
-export default App
